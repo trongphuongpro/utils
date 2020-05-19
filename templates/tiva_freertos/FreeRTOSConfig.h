bbf9ec7ffce7dd4cbabfd001d37dae8dad95744c
ntppro <mr.trongphuongpro@gmail.com>
2020-05-19 23:59:07 +0300

    Update tiva_freertos

--- templates/tiva_freertos/FreeRTOSConfig.h
+++ templates/tiva_freertos/FreeRTOSConfig.h
@@ -67,16 +67,17 @@
  *----------------------------------------------------------*/
 
 #define configUSE_PREEMPTION                1
+#define configUSE_TIME_SLICING              1
 #define configUSE_IDLE_HOOK                 0
 #define configUSE_TICK_HOOK                 0
 #define configCPU_CLOCK_HZ                  ( ( unsigned long ) 50000000 )
 #define configTICK_RATE_HZ                  ( ( portTickType ) 1000 )
-#define configMINIMAL_STACK_SIZE            ( ( unsigned short ) 200 )
-#define configTOTAL_HEAP_SIZE               ( ( size_t ) ( 30000 ) )
+#define configMINIMAL_STACK_SIZE            ( ( unsigned short ) 100 )
+#define configTOTAL_HEAP_SIZE               ( ( size_t ) ( 10000 ) )
 #define configMAX_TASK_NAME_LEN             ( 12 )
 #define configUSE_TRACE_FACILITY            1
 #define configUSE_16_BIT_TICKS              0
-#define configIDLE_SHOULD_YIELD             0
+#define configIDLE_SHOULD_YIELD             1
 #define configUSE_CO_ROUTINES               0
 #define configUSE_MUTEXES                   1
 #define configUSE_RECURSIVE_MUTEXES         1
@@ -85,6 +86,11 @@
 #define configMAX_PRIORITIES                16
 #define configMAX_CO_ROUTINE_PRIORITIES     ( 2 )
 #define configQUEUE_REGISTRY_SIZE           10
+
+#define configUSE_TIMERS                    1
+#define configTIMER_QUEUE_LENGTH            10
+#define configTIMER_TASK_PRIORITY           1
+#define configTIMER_TASK_STACK_DEPTH        500
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
@@ -97,6 +103,9 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil             1
 #define INCLUDE_vTaskDelay                  1
 #define INCLUDE_uxTaskGetStackHighWaterMark 1
+
+void showError(const char *pcFile, uint32_t line);
+#define configASSERT(x)     if ((x) == 0)   { showError(__FILE__, __LINE__); while (1);}
 
 /* Be ENORMOUSLY careful if you want to modify these two values and make sure
  * you read http://www.freertos.org/a00110.html#kernel_priority first!
